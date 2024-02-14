5683719d709b4b3ee794738c06e6a0212c865c14
laya7171 <layapandey7171@gmail.com>
2024-02-14 21:27:09 +0545

    changes

--- Lab 1/Newton.cpp
+++ Lab 1/Newton.cpp
@@ -1,80 +1,41 @@
+//Newton-Raphson Method
 #include<iostream>
 #include<cmath>
+#include<iomanip>
 using namespace std;
-
-class bisect{
-double neg;
-double pos;
-double mid;
-double error;
-double temp;
-int iteration;
-
-public:
-bisect(){
-this->error=1;
-this->temp=0;
-this->iteration=0;
+double f(double x);    
+double f(double x)    
+{
+    double a=pow(x,3.0)-x-11.0;    
+    return a;
 }
-
-void getdata(){
-cout<<"Enter the number that gives positive in the function :";
-cin>>pos;
-cout<<endl<<"Enter the number that gives negative in the function :";
-cin>>neg;
-cout<<endl;
+double fprime(double x);
+double fprime(double x)
+{
+    double b=3*pow(x,2.0)-1.0;        
+    return b;
 }
-
-double calc_error(double a,double b){
-return fabs(a-b)/a;
-
-}
-
-double findpower(double k,double l){
-if(l==0){
-return 1;
-}
-else if(l==1){
-return k;
-}
-else{
-return k*findpower(k,l-1);
-}
-}
-
-double function(double num){
-double x=sin(num)+findpower(num,2)+5*num-9;
-return x;
-}
-
-void getanswer(){
-
-do{
-++iteration;
-mid=(neg+pos)/2;
-if(function(mid)>=0){
-temp = pos;
-pos = mid;
-}
-else{
-temp = neg;
-neg = mid;
-}
-error= calc_error(mid,temp);
-}while (error>=0.005);
-
-cout<<"The no of iterations is : "<<iteration<<endl;
-
-cout<<"The root of the function is : "<<mid<<endl;
-
-}
-
-};
-
-int main(){
-bisect b;
-b.getdata();
-b.getanswer();
-return 0;
-
+int main()
+{
+    double x,x1,e,fx,fx1;
+    cout.precision(4);       
+    cout.setf(ios::fixed);    
+    cout<<"Enter the initial guess\n";    
+    cin>>x1;
+    cout<<"Enter desired accuracy\n";    
+    cin>>e;
+    fx=f(x);                
+    fx1=fprime(x);
+    cout <<"x{i}"<<"    "<<"x{i+1}"<<"        "<<"|x{i+1}-x{i}|"<<endl;                
+                     
+    do           
+    {
+        x=x1;                
+        fx=f(x);           
+        fx1=fprime(x);            
+        x1=x-(fx/fx1);            
+        cout<<x<<"     "<<x1<<"           "<<abs(x1-x)<<endl;        
+    }while (fabs(x1-x)>=e);           
+    cout<<"The root of the equation is "<<x1<<endl;
+    return 0;
 }
